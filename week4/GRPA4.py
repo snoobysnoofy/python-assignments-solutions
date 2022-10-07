bc9c07e463e65cf554318eb51268ab4e5295aa69
snoobysnoofy <114791490+snoobysnoofy@users.noreply.github.com>
2022-10-07 18:37:52 +0530

    Update GRPA4.py

--- week4/GRPA4.py
+++ week4/GRPA4.py
@@ -1,10 +1,17 @@
-names = input().split(",")
-days = input().split(",")
-common = []
-
-l = len(names)
-for i in range(l):
-    for j in range(i+1, l):
-        if i != j and days[i] == days[j] and names[i] < names[j]:
-            p = [names[i], names[j]]
-            common.append(p)
+common=[]
+a=input().split(",")
+s=input().split(",")
+b=[int(i) for i in s]
+for i in range(len(b)):
+    for j in range(i+1,len(b)):
+        if b[i]==b [j]:
+            s=[a[i],a[j]]
+            common.append(s)
+d=[]
+for i in common:
+    s=common.index(i)
+    for j in range(len(i)-1):
+        if i[j]>i[j+1]:
+            u=common[s][0]
+            common[s][0]=common[s][1]
+            common[s][1]=u
